74156a93d3e8dff1ab0c51b7354d5fbc5fc736ce
Vera Rojman <vrojman@protonmail.com>
2020-10-23 12:49:25 +0200

    Show / hide meetings sections depending on component id

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module DecidimApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.mock_meeting_id = ENV.fetch('MOCK_MEETING_ID', 5)
   end
 end
 
